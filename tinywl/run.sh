eb9b9c83e52058401af4b966ac47ca4e9ab82f7a
Karlo Lozovina <karlo@sklogw.com>
2024-01-09 03:04:52 +0100

    Move to new repository

--- tinywl/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-java                                    \
-    --enable-native-access=ALL-UNNAMED  \
-    -Dforeign.restricted=permit         \
-    --add-modules jdk.incubator.foreign \
-    src/main/java/tinywl/Tiny.java
-
